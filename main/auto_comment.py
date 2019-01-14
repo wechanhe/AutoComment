c3f4ca41c0049474fb9bece3fe1cad19e594ad09
wechanhe <1470325185@qq.com>
2019-01-14 16:44:41 +0800

    add new file

--- main/auto_comment.py
+++ main/auto_comment.py
@@ -12,6 +12,19 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
 }
 
+'''
+'MSIE (MSIE 6.0; X11; Linux; i686) Opera 7.23',
+            'Opera/9.20 (Macintosh; Intel Mac OS X; U; en)',
+            'Opera/9.0 (Macintosh; PPC Mac OS X; U; en)',
+            'iTunes/9.0.3 (Macintosh; U; Intel Mac OS X 10_6_2; en-ca)',
+            'Mozilla/4.76 [en_jp] (X11; U; SunOS 5.8 sun4u)',
+            'iTunes/4.2 (Macintosh; U; PPC Mac OS X 10.2)',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0) Gecko/20100101 Firefox/5.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20120813 Firefox/16.0',
+            'Mozilla/4.77 [en] (X11; I; IRIX;64 6.5 IP30)',
+            'Mozilla/4.8 [en] (X11; U; SunOS; 5.7 sun4u)'
+'''
 session = requests.session()
 session.headers.update(headers)
 r = session.get(login_url)
